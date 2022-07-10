8c07f68dadaea0bc264e8e56db08a36b30fada7f
Prodromos Sarakinou <p.sarakinou@gnomon.com.gr>
2022-07-10 07:43:11 +0300

    initial commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<fl_location/FlLocationPlugin.h>)
+#import <fl_location/FlLocationPlugin.h>
+#else
+@import fl_location;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlLocationPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlLocationPlugin"]];
+}
+
+@end
